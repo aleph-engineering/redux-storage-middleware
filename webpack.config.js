4072bccb90d83a327ddae330ae98100581d262fc
partner <partner@aleph.engineering>
2018-04-06 13:25:05 -0400

    Provide a name to the library property in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'reduxStorageSync.js',
+        library: 'reduxStorageSync',
         libraryTarget: 'umd',
     },
 };
