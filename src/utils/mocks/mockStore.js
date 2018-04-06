e2bdeeb85476dbcb2cb7f23cbfef7d6edf81be64
partner <partner@aleph.engineering>
2018-04-05 22:57:54 -0400

    Configure a Redux mock store for testing

---
+++ src/utils/mocks/mockStore.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import configureMockStore from 'redux-mock-store';
+
+const mockStore = configureMockStore();
+
+export default mockStore;
