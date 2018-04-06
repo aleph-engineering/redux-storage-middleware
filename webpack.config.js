3a2b145e4b5ffdf9c1d187420dab3ab98482355a
partner <partner@aleph.engineering>
2018-04-05 22:58:36 -0400

    Create webpack config file for bundling the module

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'reduxStorageSync.js'
+      }
+};
