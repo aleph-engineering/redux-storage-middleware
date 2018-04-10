277d5f8c9b89dd23b5d570f7540c819c34a3f763
partner <partner@aleph.engineering>
2018-04-10 15:05:31 -0400

    Test execution of node save code in local and session functions

--- src/utils/storage/localStorage.test.js
+++ src/utils/storage/localStorage.test.js
@@ -1,23 +1,35 @@
 import * as storage from './localStorage';
+import * as environment from './../environment';
 
 describe('Suite of tests for the local storage util', () => {
     const storageKey = 'storageKey';
+    const spyBrowser = jest.spyOn(environment, 'default');
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
     test('save should call setItem of the localStorage', () => {
         const spy = jest.spyOn(localStorage, 'setItem');
         storage.save(storageKey, {});
         expect(spy).toHaveBeenCalledTimes(1);
+        expect(spyBrowser).toHaveBeenCalledTimes(1);
         expect(spy).toHaveBeenCalledWith(storageKey, '{}');
     });
+
     test('get should call getItem of the localStorage', () => {
         const spy = jest.spyOn(localStorage, 'getItem');
         storage.get(storageKey);
         expect(spy).toHaveBeenCalledTimes(1);
+        expect(spyBrowser).toHaveBeenCalledTimes(1);
         expect(spy).toHaveBeenCalledWith(storageKey);
     });
+
     test('remove should call removeItem of the localStorage', () => {
         const spy = jest.spyOn(localStorage, 'removeItem');
         storage.remove(storageKey);
         expect(spy).toHaveBeenCalledTimes(1);
+        expect(spyBrowser).toHaveBeenCalledTimes(1);
         expect(spy).toHaveBeenCalledWith(storageKey);
     });
 });
--- src/utils/storage/sessionStorage.test.js
+++ src/utils/storage/sessionStorage.test.js
@@ -1,23 +1,35 @@
 import * as storage from './sessionStorage';
+import * as environment from './../environment';
 
 describe('Suite of tests for the session storage util', () => {
     const storageKey = 'storageKey';
+    const spyBrowser = jest.spyOn(environment, 'default');
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
     test('save should call setItem of the sessionStorage', () => {
         const spy = jest.spyOn(sessionStorage, 'setItem');
         storage.save(storageKey, {});
         expect(spy).toHaveBeenCalledTimes(1);
+        expect(spyBrowser).toHaveBeenCalledTimes(1);
         expect(spy).toHaveBeenCalledWith(storageKey, '{}');
     });
+
     test('get should call getItem of the sessionStorage', () => {
         const spy = jest.spyOn(sessionStorage, 'getItem');
         storage.get(storageKey);
         expect(spy).toHaveBeenCalledTimes(1);
+        expect(spyBrowser).toHaveBeenCalledTimes(1);
         expect(spy).toHaveBeenCalledWith(storageKey);
     });
+
     test('remove should call removeItem of the sessionStorage', () => {
         const spy = jest.spyOn(sessionStorage, 'removeItem');
         storage.remove(storageKey);
         expect(spy).toHaveBeenCalledTimes(1);
+        expect(spyBrowser).toHaveBeenCalledTimes(1);
         expect(spy).toHaveBeenCalledWith(storageKey);
     });
 });
